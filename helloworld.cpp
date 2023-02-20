e7a381a4457e69a5e4be6683900ba770e82aceec
TaraBlackburn <tara.blackburn3@gmail.com>
2023-02-19 20:49:43 -0700

    adding Hello, World!

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// Instantiate libraries just like in Python called pre processor directive:
+// input output stream = "i o stream"
+int main() {
+  
+  std::cout << "Hello World!\n";
+  // std::cout = standard character out - what they call a "c out" 
+  // "<<" that is a pointer 
+  // "\n" denoting a new line
+  // ";" ending the statement 
+  std::cout << "Welcome to My C++ Series\n";
+
+  return 0;
+}
+
+/* With VS Code and the compilers and debuggers, 
+you don't need to use a terminal to run the program
+but the output will show in the terminal for this exercise. */
+
